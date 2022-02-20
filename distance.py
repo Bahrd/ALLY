c3b40c99b214c512ff93070dca1ec1baab33db55
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2022-02-20 17:52:09 +0100

    Module class privateering

--- distance.py
+++ distance.py
@@ -1,17 +1,17 @@
-﻿### Lp distance functions implementation
-##  See: https://stackoverflow.com/questions/71091850/structural-pattern-matching-and-infinity/
+﻿### Lp distance functions implementation (incl. p = 0 and p = ∞)
 from math import sqrt, inf
 from dataclasses import dataclass
 
-# Just a helper data class/structure
+# A private helper data class/structure
+# Cf. https://stackoverflow.com/questions/71091850/structural-pattern-matching-and-infinity/
 @dataclass 
-class i: nf = inf
+class _i: nf = inf
 
 def lp_distance(x, y, p): 
     match p:
-        case 0.0:  return int(x != 0.0) + int(y != 0.0)
-        case 0.5:  return (sqrt(abs(x)) + sqrt(abs(y)))**2.0
-        case 1.0:  return abs(x) + abs(y)
-        case 2.0:  return sqrt(x**2.0 + y**2.0)
-        case i.nf: return max(abs(x), abs(y))
-        case    _: return pow(pow(abs(x), p) + pow(abs(y), p), 1.0/p)
+        case 0.0:   return int(x != 0.0) + int(y != 0.0)
+        case 0.5:   return (sqrt(abs(x)) + sqrt(abs(y)))**2.0
+        case 1.0:   return abs(x) + abs(y)
+        case 2.0:   return sqrt(x**2.0 + y**2.0)
+        case _i.nf: return max(abs(x), abs(y))
+        case     _: return pow(pow(abs(x), p) + pow(abs(y), p), 1.0/p)

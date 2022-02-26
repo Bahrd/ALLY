75b47f40358dda118d9f86204389b234c60181ae
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2022-02-26 22:24:51 +0100

    Et voilà!

--- k-NN-Lp-Voronoi.py
+++ k-NN-Lp-Voronoi.py
@@ -19,47 +19,66 @@ from PIL import Image
 from VoronoiUtilities import save_image, ITT
 
 @ITT
-def knn_lp_Voronoi(x, X, k, w, p, pattern_classes):
+def vanila_knn_lp_Voronoi(x, S, k, p):
+    X = tuple(S.keys())
+    D = cdist(array(x), array(X), 'minkowski', p = p)
+    classes = []
+    for (mn, distance) in enumerate(D): 
+        k_nearest_neighbors = argpartition(distance, k)
+        knn = [S[X[neighbor]] for neighbor in k_nearest_neighbors[:k]]
+        pattern_class = int(mode(knn).mode[0])
+        classes.append(pattern_class)
+    return classes
+
+@ITT
+def knn_lp_Voronoi(x, S, k, w, p, pattern_classes):
     # Visualization tools
     image = Image.new("RGB", (w, w)); img = image.load()
     
-    # Compute distances to every pattern w.r.t. a selected Lp function
+    # Extract patterns from the learning set S...
+    X = tuple(S.keys())
+    # ... and compute distances to every pattern w.r.t. a selected Lp function
     D = cdist(array(x), array(X), 'minkowski', p = p)
 
     # k-NN classifiers assign $x$ to the class whose index is
     # a mode of the k nearest learning patterns' classes
     for (mn, distance) in enumerate(D): 
+        # Note we don't care about the distances, we only need the class indices
         # 'k_nearest_neighbors' is a set of k nearest neighbor pattern's indices
         k_nearest_neighbors = argpartition(distance, k)
-        # 'knn' is a set of k nearest neighbor pattern's classes
+        # 'knn' is a set of k nearest neighbor pattern's class indicess
         knn = [S[X[neighbor]] for neighbor in k_nearest_neighbors[:k]]
-        # The mode determines the class the patterns are assigned to
+        # The mode determines the class index the patterns are assigned to
         pattern_class = int(mode(knn).mode[0])
 
-        ## A line below, pixel-by-pixel, 'paints a picture'
+        ## The line below 'paints a picture' pixel-by-pixel
         m, n = x[mn]; img[m, n] = pattern_classes[pattern_class]
 
     save_image(f'./images/{k}-N' + 'N-L{}@{}', image, p, sd)
 
-k, p, N, Hanan = 0b101, 0b10, 0b1000000, False
-sd, w = 0x1000000, 0x100
+# Exemplary usage
+k, p, N, Hanan = 0b111, 0b100, 0b1000000, False
 
-# For illustrative purposes
+# Colors for illustrative purposes
 c_red, c_green, c_blue, c_yellow, c_black, c_gray, c_whitish, c_white = ((0xff, 0, 0), (0, 0xff, 0), 
 																		 (0, 0, 0xff), (0xff, 0xff, 0),
 																		 (0, 0, 0), (0x80, 0x80, 0x80), 
 																		 (0xdd, 0xdd, 0xdd), (0xff, 0xff, 0xff))
 colors = [c_white, c_whitish, c_gray, c_black, c_red]
 
-# Generate patterns' coordinates (i.e. feature vectors of ${X_n} \in S_N$)
-seed(sd); 
+# Patterns' coordinates (i.e. feature vectors of ${X_n} \in S_N$)
+sd, w = 0x1000000, 0x100; seed(sd)
 nx, ny, S = [RA(10, w - 10) for _ in range(N)], [RA(10, w - 10) for _ in range(N)], {}
 
-## For a Hanan's plantation: use 'product' rather than 'zip'
+# For a Hanan's plantation: use 'product' rather than 'zip'
 patterns = product(nx, ny) if Hanan else zip(nx, ny)
-# Assign patterns at random (a number of classes should equal the size of the 'colors' list
-for x, y in patterns: S[(x, y)] = RA(len(colors))
-# Create a set of patterns {x} to be classified, then extract a set {X} of learning patterns
-x, X = tuple(product(range(w), range(w))), tuple(S.keys())
 
-knn_lp_Voronoi(x, X, k, w, p, colors)
+## Patterns assigned at random to classes 
+#  (a number of classes should equal the size of the 'colors' list
+for x, y in patterns: S[(x, y)] = RA(len(colors))
+
+# Set of patterns {x} to be classified
+x = tuple(product(range(w), range(w)))
+
+### Et voilà!
+knn_lp_Voronoi(x, S, k, w, p, colors)

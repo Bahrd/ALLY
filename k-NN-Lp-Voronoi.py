ffd73cdda6bfc15a5524a8b1dc08c06dbfc2965b
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2022-02-27 14:18:45 +0100

    Out of sorts - sorted out!

--- k-NN-Lp-Voronoi.py
+++ k-NN-Lp-Voronoi.py
@@ -10,16 +10,16 @@
 #      * a mode of class indices and assign it to $x$
 
 from numpy import arange, array, argsort, unique, zeros
+from numpy.random import randint, seed
 from scipy.spatial.distance import cdist
 from scipy.stats import mode
 from itertools import product
-from random import randrange as RA, seed
 
 from PIL import Image
 from VoronoiUtilities import save_image, ITT
 
 @ITT
-def plain_vanila_knn_lp_Voronoi(x, S, k, p):
+def plain_vanila_knn_lp_classifier(x, S, k, p):
     X = tuple(S.keys())
     D = cdist(array(x), array(X), 'minkowski', p = p)
     classes = []
@@ -49,8 +49,9 @@ def knn_lp_Voronoi(x, S, k, w, p, pattern_classes):
         k_nearest_neighbors = argsort(distance)[:k]
         # 'knn' is a set of k nearest neighbor pattern's class indicess
         knn = [S[X[neighbor]] for neighbor in k_nearest_neighbors]
-        # The mode determines the class index the patterns are assigned to
+        # The mode determines the class index the patterns are assigned to...
         unique_knn = len(unique(knn))
+        # ... or the first one if there are more modes
         pattern_class = int(mode(knn).mode[0]) if unique_knn != k else int(knn[0])
 
         ## The line below 'paints a picture' pixel-by-pixel
@@ -59,7 +60,7 @@ def knn_lp_Voronoi(x, S, k, w, p, pattern_classes):
     save_image(f'./images/{k}-N' + 'N-L{}@{}', image, p, sd)
 
 # Exemplary usage
-k, p, N, Hanan = 0b11, 0b1, 0b10_000, False
+k, p, N, Hanan = 0b11, 0b10, 0b1_000_000, False
 
 # Colors for illustrative purposes
 c_red, c_green, c_blue, c_yellow, c_black, c_gray, c_whitish, c_white = ((0xff, 0, 0), (0, 0xff, 0), 
@@ -70,17 +71,20 @@ colors = [c_white, c_whitish, c_gray, c_black, c_red]
 
 # Patterns' coordinates (i.e. feature vectors of ${X_n} \in S_N$)
 sd, w = 0x1_000_000, 0x100; seed(sd)
-nx, ny, S = [RA(0x10, w - 0x10) for _ in range(N)], [RA(0x10, w - 0x10) for _ in range(N)], {}
+nx, ny, S = randint(0o10, w - 0o10, N), randint(0o10, w - 0o10, N), {}
 
 # For a Hanan's plantation: use 'product' rather than 'zip'
 patterns = product(nx, ny) if Hanan else zip(nx, ny)
 
 ## Patterns assigned at random to classes 
 #  (a number of classes should equal the size of the 'colors' list
-for x, y in patterns: S[(x, y)] = RA(len(colors))
+for x, y in patterns: S[(x, y)] = randint(len(colors))
 
 # Set of patterns {x} to be classified
 x = tuple(product(range(w), range(w)))
 
 ### Et voilà!
 knn_lp_Voronoi(x, S, k, w, p, colors)
+
+# ... and a (nutshell) summary and fanfares!
+print('seed =', sd) # A tribute to CDMA (and H. Lamar & G. Antheil 1942's invention)
